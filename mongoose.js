d87cd3b23bec0bfd1112b807b19386724bf8a714
Omkar Devrukhkar <omkardevrukhkar0@gmail.com>
2025-05-20 03:19:14 +0530

    mongo url changes

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,3 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    "mongodb+srv://omkardevrukhkar0:xXqIkV6UNGegWSMF@elred.wtabeyp.mongodb.net/?retryWrites=true&w=majority&appName=elRed"
-  )
-  .then(() => console.log("Connected!"));
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("Connected!"));
